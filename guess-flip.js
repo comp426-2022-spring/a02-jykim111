12ba998ad704eaa0d4949f4811e44b29791d79e4
jykim111 <jykim1@email.unc.edu>
2022-02-23 23:49:37 -0500

    Finished guess-flip.js

---
+++ guess-flip.js
@@ -0,0 +1,15 @@
+import * as coin from "./modules/coin.mjs";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const args = require('minimist')(process.argv.slice(2));
+args['call'];
+
+const call = args.call;
+
+if (call == null) {
+    console.log("Error: no input.");
+    console.log("Usage: node guess-flip --call=[head|tails]");
+} else {
+    console.log(coin.flipACoin(call));
+}
